c04cbd8fd29895a4c44433a816c31a55919fcf58
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-21 19:41:56 +0600

    added login.js

---
+++ script/login.js
@@ -0,0 +1,25 @@
+// alert()
+// console.log('hero alom')
+// step 1 get the id via getelementbyId
+document.getElementById('login-btn').addEventListener('click', function(event){
+    // console.log(event);
+    event.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
+    // console.log(accountNumber);
+    
+    const pin= document.getElementById('pin').value;
+    const convertedPin =  parseInt(pin)
+    // console.log(accountNumber,pin);
+        if(accountNumber.length===11){
+            if(convertedPin===1234){
+                console.log('ok to proceed');
+            }
+            else{
+                console.log('pin is wrong');
+                
+            }
+        }else{
+            console.log('Need valid account number')
+        }
+    
+})
