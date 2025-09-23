79e3163e7139c94c64d17e6522ee9c55b372ff26
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-24 02:42:54 +0600

    added toggle function:

--- script/toggle.js
+++ script/toggle.js
@@ -2,11 +2,25 @@
 document.getElementById("cashout").style.display = "none";
 
 document.getElementById("add-money-box").addEventListener("click", function () {
-  document.getElementById("cashout").style.display = "none";
-  document.getElementById("addmoney").style.display = "block";
+
+  handleToggle("addmoney", "block")
+  handleToggle("cashout", "none")
+  // document.getElementById("cashout").style.display = "none";
+  // document.getElementById("addmoney").style.display = "block";
 });
 
 document.getElementById("cashout-box").addEventListener("click", function () {
-  document.getElementById("cashout").style.display = "block";
-  document.getElementById("addmoney").style.display = "none";
+  
+  handleToggle("addmoney", "none")
+  handleToggle("cashout", "block")
+  // document.getElementById("cashout").style.display = "block";
+  // document.getElementById("addmoney").style.display = "none";
 });
+
+
+function handleToggle(id, status) {
+  document.getElementById(id).style.display = status;
+}
+
+
+
