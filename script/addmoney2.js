f521f872f7d3fb56ab199d72f735ed3cceae64aa
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-24 12:17:07 +0600

    done with transaction

--- script/addmoney2.js
+++ script/addmoney2.js
@@ -1,3 +1,5 @@
+
+
 document
   .getElementById("add-money")
   .addEventListener("click", function (event) {
@@ -9,6 +11,17 @@ document
 
     // console.log(mainBalace);
 
+    const selectedBank = document.getElementById('allbank').value;
+    // console.log(selectedBank);
+    
+
+    if(amount<0){
+      alert('add a positive number please')
+      return;
+    }
+
+    
+
     // console.log(amount, pin);
 
     if (account.length === 11) {
@@ -17,13 +30,27 @@ document
         // document.getElementById('main-balance').innerText=sum;
         setInnerTextByIDandValue("main-balance", sum);
 
-        const container = document.getElementById('t-container')
-        const p = document.createElement("p");
-        p.innerText = `
-        added ${amount} from ${account}
-        `;
+        const container = document.getElementById('t-container');
+
+        const div = document.createElement('div'); 
+        div.classList.add('bg-red-400')
+        div.innerHTML = `
+          <h1 class='text-yellow-300'>
+          added money from ${selectedBank}
+          </h1>
+          <h3>${amount}</h3>
+          <p>account number: ${account}</p>
+          
+        `
+        container.appendChild(div)
+
+        // const container = document.getElementById('t-container')
+        // const p = document.createElement("p");
+        // p.innerText = `
+        // added ${amount} from ${account}
+        // `;
         // console.log(p);
-        container.appendChild(p)
+        // container.appendChild(p)
         
       } else {
         console.log("wrong pin");
--- script/cashout.js
+++ script/cashout.js
@@ -1,37 +1,48 @@
-
-
-document.getElementById('cashout-btn').addEventListener('click',function(event){
+document
+  .getElementById("cashout-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    const accountNumber = document.getElementById('account-number').value;
-    const amount = getInputValueByID('cashout-amount')
-    const pinNumber = getInputValueByID('cashout-pin')
-    const mainBalace = getInnerTextID('main-balance')
-    
+    const accountNumber = document.getElementById("account-number").value;
+    const amount = getInputValueByID("cashout-amount");
+    const pinNumber = getInputValueByID("cashout-pin");
+    const mainBalace = getInnerTextID("main-balance");
 
-    if(accountNumber.length===11){
-        if (pinNumber === 1234) {
-            const sum = mainBalace - amount;
-            setInnerTextByIDandValue('main-balance', sum)
+    if (amount > mainBalace) {
+      alert("invalid amount");
+      return;
+    }
 
-            const container = document.getElementById('t-container')
-        const p = document.createElement("p");
-        p.innerText = `
-        cashout ${amount} from this ${accountNumber} account
-        `;
+    if (accountNumber.length === 11) {
+      if (pinNumber === 1234) {
+        const sum = mainBalace - amount;
+        setInnerTextByIDandValue("main-balance", sum);
+
+
+                const container = document.getElementById('t-container')
+                const div = document.createElement('div');
+                div.classList.add('bg-red-400')
+                div.innerHTML = `
+                  <h1 class='text-yellow-300'>
+                  Cashout done
+                  </h1>
+                  <h3>${amount}</h3>
+                  <p>account number: ${accountNumber}</p>
+                  
+                `
+                container.appendChild(div)
+        // const container = document.getElementById("t-container");
+        // const p = document.createElement("p");
+        // p.innerText = `
+        // cashout ${amount} from this ${accountNumber} account
+        // `;
         // console.log(p);
-        container.appendChild(p)
-        }
-        else{
-            alert('pin not valid')
-        }
+        // container.appendChild(p);
+      } else {
+        alert("pin not valid");
+      }
+    } else {
+      alert("accout number not valid");
     }
-    else{
-        alert('accout number not valid')
-    }
-
-
-
-
 
     // const pin = document.getElementById('cashout-pin').value;
     // const convertedPin = parseInt(pin);
@@ -47,9 +58,9 @@ document.getElementById('cashout-btn').addEventListener('click',function(event){
     //     const sum = convertedMainBalance - convertedAmount;
     //     // console.log(sum);
     //      document.getElementById('main-balance').innerText= sum;
-        
+
     // }
     // else {
     //     alert("enter valid pin")
     // }
-})
+  });
